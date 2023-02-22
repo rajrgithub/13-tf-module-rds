e160daf770303a6986a7104c3e7e9cf238fb6840
brajasekharreddy <raj479.mits@gmail.com>
2023-02-22 05:59:59 +0530

    rds for mysql database

---
+++ vars.tf
@@ -0,0 +1,9 @@
+variable "env" {}
+variable "subnet_ids" {}
+variable "vpc_id" {}
+variable "allow_cidr" {}
+variable "engine_version" {}
+variable "number_of_instances" {}
+variable "instance_class" {}
+variable "engine" {}
+
