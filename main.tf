f4a5c3d9757c4d0c2f226627ae8a3bc1e07b7080
brajasekharreddy <raj479.mits@gmail.com>
2023-02-26 23:01:10 +0530

    rds endpoint

--- main.tf
+++ main.tf
@@ -53,3 +53,9 @@ resource "aws_rds_cluster" "rds" {
   )
 
 }
+
+resource "aws_ssm_parameter" "rds_endpoint" {
+  name  = "${var.env}.rds.ENDPOINT"
+  type  = "String"
+  value = aws_rds_cluster.rds.endpoint
+}
